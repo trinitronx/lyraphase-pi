8d5d151da234b926c90d939f2538f39809869b30
James Cuzella <james.cuzella@lyraphase.com>
2016-07-02 23:53:15 -0600

    Run ChefSpec::Coverage & add fauxhai hints in prep for customink/fauxhai#238 release

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+## Wait for PR (customink/fauxhai#238) to use the Fauxhai data for ChefSpec
+## Reference: https://github.com/customink/fauxhai/pull/238
+#RSpec.configure do |config|
+#  config.platform = 'raspbian'
+#  config.version = '8.0'
+#end
+
+at_exit { ChefSpec::Coverage.report! }
