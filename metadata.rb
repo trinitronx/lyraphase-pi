b1ffeb85e2879032ce7c0fcc7eda4ab771dd9500
James Cuzella <james.cuzella@lyraphase.com>
2016-07-11 21:07:11 -0600

    Bump cookbook to v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'james.cuzella@lyraphase.com'
 license 'gplv3'
 description 'Installs/Configures lyraphase-pi'
 long_description 'Installs/Configures lyraphase-pi'
-version '0.1.0'
+version '0.2.0'
 issues_url 'https://github.com/trinitronx/lyraphase-pi/issues'
 source_url 'https://github.com/trinitronx/lyraphase-pi'
 
