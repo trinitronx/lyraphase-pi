7154affdaf216330a60946b643755fd6dc28bc6f
James Cuzella <james.cuzella@lyraphase.com>
2016-07-11 22:57:16 -0600

    wifi-bridge: Fix ServerSpec test fixtures path, Check all network scripts

--- test/integration/wifi-bridge/serverspec/wifi-bridge_spec.rb
+++ test/integration/wifi-bridge/serverspec/wifi-bridge_spec.rb
@@ -17,8 +17,11 @@ describe 'lyraphase-pi::wifi-bridge' do
     [ 'avahi/avahi-daemon.conf',
       'default/dhcp-helper',
       'network/interfaces',
-      'network/interfaces.d/wireless-bridge-dhcp-parprouted' ].each do |etc_file_path|
-      fixture_path = File.join(File.dirname(__FILE__), '..', 'fixtures', etc_file_path.split('/') )
+      'network/interfaces.d/wireless-bridge-dhcp-parprouted',
+      'systemd/system/parprouted.service',
+      'systemd/system/wpa-cli-event-handler.service'
+    ].each do |etc_file_path|
+      fixture_path = File.join(File.dirname(__FILE__), '..', 'test', 'fixtures', etc_file_path.split('/') )
       describe file(File.join('', 'etc', etc_file_path.split('/'))) do
         it { should be_file }
         its(:content) { should eq(File.open(fixture_path, 'r').read) }
@@ -29,13 +32,25 @@ describe 'lyraphase-pi::wifi-bridge' do
     end
   end
 
-  ['setup', 'cleanup'].each do |script|
-    describe "installs wireless-bridge-#{script} for ifup / ifdown" do
-      describe file(File.join('', 'etc', 'network', "wireless-bridge-#{script}")) do
+  [ 'network/wireless-bridge-setup',
+    'network/wireless-bridge-cleanup',
+    'network/wireless-bridge-ip-clone',
+    'network/wpa-supplicant-event-handler'
+  ].each do |script|
+    fixture_path = File.join(File.dirname(__FILE__), '..', 'test', 'fixtures', script.split('/') )
+    describe "installs /etc/#{script} for ifup / ifdown" do
+      describe file(File.join('', 'etc', script)) do
         it { should be_owned_by 'root' }
         it { should be_grouped_into 'root' }
         it { should be_mode '755' }
+        its(:content) { should eq(File.open(fixture_path, 'r').read) }
       end
+    end
+  end
+
+  describe "Linux Kernel IPv4 Packet Forwarding" do
+    describe linux_kernel_parameter('net.ipv4.ip_forward') do
+      its(:value) { should eq 1 }
     end
   end
 
