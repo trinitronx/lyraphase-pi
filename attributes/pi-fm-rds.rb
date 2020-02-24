fd3a6bdd6d6426844fb42c75da332a4b2e47592c
James Cuzella <james.cuzella@lyraphase.com>
2020-02-24 03:18:23 -0700

    Adding pi-fm-rds initial recipe

---
+++ attributes/pi-fm-rds.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+default['lyraphase-pi']['pi-fm-rds']['packages'] = ['libsndfile1-dev']
+default['lyraphase-pi']['pi-fm-rds']['git_repository'] = 'https://github.com/ChristopheJacquet/PiFmRds.git'
+default['lyraphase-pi']['pi-fm-rds']['git_revision'] = 'b0f253232b2c5084c51dc81200e6d1a116221ff2'
